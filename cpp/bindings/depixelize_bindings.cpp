7665a7b16d91021979b1378a12ccf708992f8383
Ewout van Mansom <ewout@vanmansom.name>
2026-03-12 06:51:47 +0100

    bugfix: revert SVG stroke painting

--- cpp/bindings/depixelize_bindings.cpp
+++ cpp/bindings/depixelize_bindings.cpp
@@ -152,7 +152,7 @@ static std::string depixelize(
     svg += std::to_string(splines.width());
     svg += "\" height=\"";
     svg += std::to_string(splines.height());
-    svg += "\">\n<style>path{shape-rendering:crispEdges;stroke-width:.05;paint-order:stroke}</style>\n";
+    svg += "\">\n<style>path{shape-rendering:crispEdges}</style>\n";
 
     for (const auto& path : splines) {
         std::string d = path_to_svg(path.pathVector);
@@ -164,8 +164,6 @@ static std::string depixelize(
         svg += "<path d=\"";
         svg += d;
         svg += "\" fill=\"";
-        svg += hex;
-        svg += "\" stroke=\"";
         svg += hex;
         svg += '"';
         if (opacity < 1.0) {
